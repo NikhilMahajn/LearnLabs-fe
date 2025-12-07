0ab6e6781a626076ee82fd65700ea6e77a69946b
NikhilMahajn <mahajannikhil0016@gmail.com>
2025-12-07 12:06:54 +0530

    analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,18 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/authContext.jsx'
 import { LoaderProvider } from './context/loaderContext.jsx'
+import { Analytics } from "@vercel/analytics/next"
 
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  
   <BrowserRouter>
     <AuthProvider>
       <LoaderProvider>
         <App />
+        <Analytics/>
       </LoaderProvider>
     </AuthProvider>
   </BrowserRouter>
